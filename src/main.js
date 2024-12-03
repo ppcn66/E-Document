33e626bedbfaca3b465231177e68d90771404fd5
ppcn66 <ppcn.66@gmail.com>
2024-12-03 23:39:08 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
